03a36fc21e49f5ff3946e8189c83d0d2e5e526d2
Samir Cəbrayıllı <samirjabrailli98@gmail.com>
2024-07-19 17:28:06 +0400

    Unsplash API

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const API_KEY = "6TSvZ8L_ml4NBk7fHeEGQdkgl0asqwIgIj0rEvYq0zI";
+  const [query, setQuery] = useState('cat');
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetchPhotos(query);
+  }, [query]);
+
+  const fetchPhotos = async (query) => {
+    try {
+      const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+        params: { query },
+        headers: {
+          Authorization: `Client-ID ${API_KEY}`
+        }
+      });
+      setPhotos(response.data.results);
+    } catch (error) {
+      console.error("Error fetching photos:", error);
+    }
+  };
+
+  const handleSearch = () => {
+    const input = document.getElementById('searchInput').value;
+    setQuery(input);
+  };
+
+  return (
+    <div className="container">
+      <input type="text" id="searchInput" defaultValue={"cat"}  />
+      <button onClick={handleSearch}>Search</button>
+      <div className="photo-grid">
+        {photos.map((photo) => (
+          <div key={photo.id} className="photo-item">
+            <img src={photo.urls.small} alt={photo.alt_description} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
